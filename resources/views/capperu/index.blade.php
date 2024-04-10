a15d18d3ef8aebe96f92fa3e9ed98fa8a3cfc5fd
Yisus Web <connexion.jesus@gmail.com>
2024-04-10 00:48:41 -0500

    buscador resultado

--- resources/views/capperu/index.blade.php
+++ resources/views/capperu/index.blade.php
@@ -211,7 +211,7 @@
                             </span>
                         </h1>
                         <div class="newslatter-inner mt-xl-4 me-xl-5">
-                            <input type="text" placeholder="Ingresar DNI del alumno">
+                            <input type="text" placeholder="Ingresar nombres y apellidos del alumno">
                             <button class="btn btn-base">Buscar</button>
                         </div>
                         <div class="banner-multi-user mt-xl-5 mt-4">
@@ -227,6 +227,48 @@
                     </div>
                 </div>
                 <div class="col-md-3"></div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-8">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nombres</th>
+                            <th scope="col">Apellidos</th>
+                            <th scope="col">...</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>
+                                <button class="btn-small btn-primary">Ver</button>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>
+                                <button class="btn-small btn-primary">Ver</button>
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>
+                                <button class="btn-small btn-primary">Ver</button>
+                            </td>
+                          </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-2"></div>
             </div>
         </div>
     </section>
