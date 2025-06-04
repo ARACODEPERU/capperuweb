dcd54a4e605a2d33d29806bf17af6421420f32f9
Grandejosh <grande_josh@odontologos.com>
2025-06-03 23:46:07 -0500

    d
    d

--- Modules/Onlineshop/Http/Controllers/OnliItemController.php
+++ Modules/Onlineshop/Http/Controllers/OnliItemController.php
@@ -326,6 +326,7 @@ class OnliItemController extends Controller
             $OnliItem->additional2 = $additional2;
         }
 
+        $OnliItem->additional2 = $request->get('additional2');
         $OnliItem->additional3 = $request->get('additional3');
         $OnliItem->additional4 = $request->get('additional4');
         $OnliItem->additional5 = $request->get('additional5');
