ece8993d1721c86d7d8c7a568af3c5095ee795ca
Yisus Web <connexion.jesus@gmail.com>
2025-12-11 17:10:20 -0500

    fonfo blanco

--- resources/views/pages/program-description.blade.php
+++ resources/views/pages/program-description.blade.php
@@ -71,7 +71,9 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         @if ($course->brochure)
+                                            <div class="overview-area">
                                                 {!! $course->brochure->curriculum_plan !!}
+                                            </div>
                                         @endif
                                         {{-- @php
                                             $modules = $course->modules;
